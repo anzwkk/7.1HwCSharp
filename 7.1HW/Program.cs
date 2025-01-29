2a39fd1998751c318693bd998b5af7fcfd181f49
anzwkk <anam89737@gmail.com>
2025-01-29 22:51:04 +0300

    Added CheckWin and PrintBoard methods, checking for a win, and updating the board after a move

--- 7.1HW/Program.cs
+++ 7.1HW/Program.cs
@@ -10,6 +10,7 @@ class Program
         bool gameRunning = true;
         while (gameRunning)
         {
+            PrintBoard();
             Console.Write($"Гравець {currentPlayer}, виберіть клітинку (1-9): ");
             if (int.TryParse(Console.ReadLine(), out move))
             {
@@ -18,12 +19,47 @@ class Program
                     if (board[move - 1] == (char)(move + '0'))
                     {
                         board[move - 1] = currentPlayer;
+
+                        if (CheckWin())
+                        {
+                            PrintBoard();
+                            Console.WriteLine($"Переміг гравець {currentPlayer}!");
+                            gameRunning = false;
+                        }    
                     }
 
                 }
             }    
 
         }
+    }
+    static bool CheckWin()
+    {
+        int[,] win = new int[,]
+        {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}
+        };
+
+        for (int i = 0; i < win.GetLength(0); i++)
+        {
+            if (board[win[i, 0]] == currentPlayer &&
+                board[win[i, 1]] == currentPlayer &&
+                board[win[i, 2]] == currentPlayer)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    static void PrintBoard()
+    {
+        Console.WriteLine($" {board[0]} | {board[1]} | {board[2]} ");
+        Console.WriteLine("-----------");
+        Console.WriteLine($" {board[3]} | {board[4]} | {board[5]} ");
+        Console.WriteLine("-----------");
+        Console.WriteLine($" {board[6]} | {board[7]} | {board[8]} ");
     }
 }
 
