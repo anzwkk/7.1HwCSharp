bd2b07249d22213c1809b2c71e59e6199aa537ef
anzwkk <anam89737@gmail.com>
2025-01-29 22:23:53 +0300

    Added player move entry with check for cell availability

--- 7.1HW/Program.cs
+++ 7.1HW/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    static char currentPlayer = 'X';
+    static void Main()
+    {
+        int move;
+        bool gameRunning = true;
+        while (gameRunning)
+        {
+            Console.Write($"Гравець {currentPlayer}, виберіть клітинку (1-9): ");
+            if (int.TryParse(Console.ReadLine(), out move))
+            {
+                if (move >= 1 && move <= 9)
+                {
+                    if (board[move - 1] == (char)(move + '0'))
+                    {
+                        board[move - 1] = currentPlayer;
+                    }
+
+                }
+            }    
+
+        }
+    }
+}
+
